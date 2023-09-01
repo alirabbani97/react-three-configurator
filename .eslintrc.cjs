ba36def15783e75e16934da6d6d2e09987918e50
ali <alirabbani54@gmail.com>
2023-09-01 17:20:37 +0500

    client

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-<<<<<<< HEAD
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
@@ -22,21 +21,3 @@ module.exports = {
     "react/jsx-uses-vars": "error",
   },
 };
-=======
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
->>>>>>> 630a8c08c6e538b21d7edaba42ad2047325503eb
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-=======
-  content: [],
->>>>>>> 630a8c08c6e538b21d7edaba42ad2047325503eb
   theme: {
     extend: {},
   },
